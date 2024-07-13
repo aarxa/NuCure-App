bef5d57de6fb0bb5e892432f71171bd615685c0b
Aarya Rajgor <aaryarajgor26@gmail.com>
2024-07-13 01:05:10 -0700

    Updated Files

---
+++ constants/index.ts
@@ -0,0 +1,54 @@
+export const GenderOptions = ["Male", "Female", "Other"];
+
+export const PatientFormDefaultValues = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  phone: "",
+  birthDate: new Date(Date.now()),
+  gender: "Male" as Gender,
+  address: "",
+  occupation: "",
+  emergencyContactName: "",
+  emergencyContactNumber: "",
+  primaryPhysician: "Dr. Deviyani Rajgor",
+  insuranceProvider: "",
+  insurancePolicyNumber: "",
+  allergies: "",
+  currentMedication: "",
+  familyMedicalHistory: "",
+  pastMedicalHistory: "",
+  identificationType: "Birth Certificate",
+  identificationNumber: "",
+  identificationDocument: [],
+  treatmentConsent: false,
+  disclosureConsent: false,
+  privacyConsent: false,
+};
+
+export const IdentificationTypes = [
+  "Birth Certificate",
+  "Driver's License",
+  "Medical Insurance Card/Policy",
+  "Aadhar Card",
+  "Passport",
+  "Student ID Card",
+  "Voter ID Card",
+];
+
+export const Doctors = [
+  {
+    image: "/assets/images/dr-green.png",
+    name: "Dr. Deviyani Rajgor",
+  },
+  {
+    image: "/assets/images/dr-cameron.png",
+    name: "Rajesh Rajgor",
+  },
+];
+
+export const StatusIcon = {
+  scheduled: "/assets/icons/check.svg",
+  pending: "/assets/icons/pending.svg",
+  cancelled: "/assets/icons/cancelled.svg",
+};
